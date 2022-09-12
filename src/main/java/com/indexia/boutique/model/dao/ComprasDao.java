ccd5ed9adda4714b32c0d6ae7cf65a6f60e9770d
Mario <oiram90140@gmail.com>
2022-09-12 10:23:53 -0500

    nuevo servicio de busqueda por id de medio de pago

--- src/main/java/com/indexia/boutique/model/dao/ComprasDao.java
+++
@@ -1,8 +0,0 @@
-package com.indexia.boutique.model.dao;
-
-import com.indexia.boutique.model.entity.Orden;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ComprasDao extends JpaRepository<Orden, Integer> {
-
-}
